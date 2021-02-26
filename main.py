6d5553806265df4ed973f1e514a865402d4960ad
Luca Mondini <lucamondini78@gmail.com>
2021-02-26 13:32:55 +0100

    Random forest

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import os
+from sklearn.ensemble import RandomForestClassifier
 
 # Input data files are available in the "../input/" directory.
 
@@ -27,3 +28,17 @@ men = train_data.loc[train_data.Sex == 'male']["Survived"]
 rate_men = sum(men)/len(men)
 
 print("% of men who survived:", rate_men)
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
